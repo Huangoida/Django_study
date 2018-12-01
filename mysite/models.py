3980b91a830869c59afe8ea3bbf3f19654ffad53
Oida <617500087@qq.com>
2018-12-01 16:04:49 +0800

    Added view and template for detailed blog post as well as CSS for the site.

--- mysite/models.py
+++ mysite/models.py
@@ -14,6 +14,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(blank=True,null=True)
+    publish_date = models.DateTimeField(blank=True,null=True)
 
     def publish(self):
         self.published_date = timezone.now()
